f5fe2d5374e888787dd3dcd9dc7625987cac2fe0
ericjohannes <eric.j.blom@gmail.com>
2021-12-17 09:00:16 -0500

    refactored to make it simpler

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -3,49 +3,55 @@ import {Button} from '@material-ui/core';
 import TextField from "@material-ui/core/TextField";
 
 
-const makeTextField = (value, changeFunc, label, testid) =>{
+const makeTextField = (value, onChange, state) =>{
+    const testid = value + '-field'
+    const label = value.charAt(0).toUpperCase() + value.slice(1)
+
     return  (
         <TextField
-            onChange={changeFunc}
-            value={value}
+            onChange={onChange}
+            value={state[value]}
             label={label}
             data-testid={testid}
+            key={value+'textfield'}
         />
     )
 }
 
-const result = (value, label) =>{
+const result = (value, state) =>{
+    const label = value.charAt(0).toUpperCase() + value.slice(1)
     return (
-        <>
-            <label>{label}</label>
-            <h3>{value}</h3>
-        </>
+        <div key={value+"fragment"}>
+            <label key={value+'label'}>{label}</label>
+            <p key={value+'p'}>{state[value]}</p>
+        </div>
     )
 }
 const LoginForm = () => {
     const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
+    const [data, setData] = useState({})
     const [submitted, setSubmitted] = useState(false)
-    
-    const onNameChange = (e) => setName(e.target.value);
-    const onPasswordChange = (e) => setPassword(e.target.value);
-
     const handleSubmit = () => setSubmitted(true)
     const handleReset = () => {
         setSubmitted(false);
-        setName("");
-        setPassword("")
+        setData({})
     }
-    let content = [makeTextField(name, onNameChange, 'Name', 'name-field'), makeTextField(password, onPasswordChange, 'Password', 'password-field')]
-    if(submitted){
-        content = [result(name, 'Name'), result(password, 'Password')]
+
+    const items = ['name', 'password']
+    const wrapperFunc = (item, state)=>{
+        if(submitted){
+            return result(item, state)
+        }else{
+            const changeFunc = (e)=>setData(Object.assign(state, { [item] :e.target.value}))
+            return makeTextField(item, changeFunc, state)
+        }
     }
     return (
         <div className="container">
             <h1>Login</h1>
             <div>
                 <div className='form'>
-                    {content.map((x, i)=><div key={"div"+i}>{x}</div>)}
+                    {items.map((x, i)=>wrapperFunc(x, data))}
                 </div>
                 <Button onClick={handleSubmit}>Submit</Button>
                 <Button onClick={handleReset}>Reset</Button>
